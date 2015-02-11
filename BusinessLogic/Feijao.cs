d5418968bc1b54ae4ff089281bc38f90f9391042
Pablo Custodio <pablocustodio@hotmail.com>
2015-02-11 20:33:11 -0200

    Inicio projeto

---
+++ BusinessLogic/Feijao.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BusinessLogic
+{
+    public class Feijao : IAlimento
+    {
+        public Feijao()
+        {
+            throw new System.NotImplementedException();
+        }
+    
+        public float QtdeGramas
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        private float _qtdeGramas
+        {
+            set { throw new NotImplementedException(); }
+        }
+
+        public void Preparar()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
